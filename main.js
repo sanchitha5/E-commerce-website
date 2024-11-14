74a961912d733c3339298a88c67f517a6c67894f
Sanchitha Poojari <sanchithaspoojari22@gmail.com>
2024-11-14 18:48:18 +0530

    SEcond Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navlinks = document.getElementById("navlinks");
+
+hamburger.addEventListener("click", ()=> {
+    
+    navlinks.classList.toggle("navlinks-active");
+
+})
